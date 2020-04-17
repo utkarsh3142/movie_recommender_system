fd9b62f1fc7b6287a1b11ec6ad7963f18b12c0bf
Parth Naik <parthnaik2011@gmail.com>
2020-04-16 21:12:13 -0400

    Predict.py and model files for 10k data

---
+++ src/predict.py
@@ -0,0 +1,43 @@
+# Get in-data movie suggestions using saved model files
+
+# Import dependencies
+import pickle
+import numpy as np
+
+# Load mapping dictionaries
+with open('model/movie_to_idx.pickle', 'rb') as handle:
+    movie_to_idx = pickle.load(handle)
+
+with open('model/link_to_idx.pickle', 'rb') as handle:
+    link_to_idx = pickle.load(handle)
+
+with open('model/idx_to_movie.pickle', 'rb') as handle:
+    idx_to_movie = pickle.load(handle)
+
+movie_encodings = np.load('model/movie_encodings.npy')
+
+# Movie list
+movies = movie_to_idx.keys()
+
+# Get similar movies using the embeddings
+def getSimilarMovies(movie_name, similar_num):
+  movie_idx = movie_to_idx[movie_name]
+  movie_encoding = movie_encodings[movie_idx]
+  
+  # Calculate similarities with rest of the movies
+  similarities = []
+  for i in range(len(movies)):
+    if i != movie_idx:
+      similarities.append((i,np.square(movie_encodings[i] - movie_encoding).mean(axis = 0)))
+  
+    # Sort by similarities  
+  similarities = sorted(similarities, key = lambda x: x[1])
+
+  # Take top similar_num matches
+  similar_movies = []
+  for i in similarities[:similar_num]:
+    similar_movies.append(idx_to_movie[i[0]])
+
+  return similar_movies
+
+print(getSimilarMovies('Deadpool (film)',10))
